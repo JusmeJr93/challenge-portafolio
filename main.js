25cbb2446c4a5933209ae7c2607693bd9355fc2a
Junior Jusme <junior.jusme@gmail.com>
2024-04-17 20:27:47 -0600

    Adding links

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ window.addEventListener('scroll', function () {
     }
 });
 
+// Función para cerrar el menú de navegación en celulares y tablet
+document.addEventListener("DOMContentLoaded", function () {
+    // Seleccionar todos los enlaces dentro del navbar
+    let links = document.querySelectorAll('#main-navbar .nav-link');
+
+    // Función para cerrar el menú de navegación
+    function closeNavbar() {
+        let navbarCollapse = document.querySelector('.navbar-collapse.collapse');
+        // Si está utilizando Bootstrap 5, puede utilizar las funciones de utilidad de Bootstrap
+        if (navbarCollapse.classList.contains('show')) {
+            new bootstrap.Collapse(navbarCollapse, {
+                toggle: false
+            }).hide();
+        }
+    }
+
+    // Añade un evento de clic a cada enlace para cerrar el menú al hacer clic
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            closeNavbar();
+        });
+    });
+});
